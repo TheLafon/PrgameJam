dd8e0fd8a893e54dc2a8fc010093ede7e6e85b0a
TheLafon <Lawrence.A.Seabrook@gmail.com>
2024-07-19 03:16:34 -0400

    Added Several Player Abilitys
    The Player can now Wall Jump
    The Player can now Attack
    The Player can now Dash (this will be updated to be smoother soon)
    A State Machine for the Player was created

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,21 +1,41 @@
 extends CharacterBody2D
 
 
-var input
+var input = 0.0
+
+#jump related variables
 var jump_count = 0
 @export var max_jump = 2
 @export var jump_force = 400
+
 @export var speed = 100.0
 @export var gravity = 10
 
+#WallJump Variables
+@onready var wall = $wall_ray
 
+#State Machine information
+
+#dashing Variable 
+@export var dash_force = 600
+
+#State Machine for the Player
+var current_state = player_states.MOVE
+enum player_states {MOVE, ATTACK, DEAD, DASH}
 
 
 func _ready():
-	pass
+	$Sword/Attack_collider.disabled = true
 
-func _process(delta):
-	movement(delta)
+func _physics_process(delta):
+	#triggers state
+	match current_state:
+		player_states.MOVE:
+			movement(delta)
+		player_states.ATTACK:
+			Attack(delta)
+		player_states.DASH:
+			dashing()
 	
 func movement(delta):
 	#handles left and right directionals
@@ -28,12 +48,16 @@ func movement(delta):
 			velocity.x = clamp(speed, 100.0, speed)
 			#controls the animations and fliping of the character
 			$AnimatedSprite2D.scale.x = 1
+			$Sword.position.x = -12
+			wall.scale.x = 1
 			$Anim.play("Run")
 		if input < 0:
 			velocity.x -= speed * delta
 			velocity.x = clamp(-speed, 100.0, -speed)
 			#controls the animations and fliping of the character
 			$AnimatedSprite2D.scale.x = -1
+			$Sword.position.x = -36
+			wall.scale.x = -1
 			$Anim.play("Run")
 	if input == 0:
 		velocity.x = 0
@@ -42,6 +66,7 @@ func movement(delta):
 		#handles jump
 	if is_on_floor():
 		jump_count = 0
+	
 	
 	if !is_on_floor():
 		#afirms that the player is not on the floor
@@ -53,7 +78,7 @@ func movement(delta):
 
 		#counts the jump value and counts if the player is on the floor and
 		#if the jump count is less then the max jumps allowed
-	if Input.is_action_pressed("Jump") && is_on_floor() && jump_count < max_jump:
+	if Input.is_action_just_pressed("Jump") && is_on_floor() && jump_count < max_jump:
 		jump_count += 1
 		velocity.y -= jump_force
 		velocity.x = input
@@ -72,9 +97,82 @@ func movement(delta):
 	else:
 		gravity_force()
 		
+		
+	if wall_collide() && Input.is_action_just_pressed("Jump"):
+		if velocity.x > 0:
+			velocity = Vector2(-800, -350)
+		elif velocity.x < 0:
+			velocity = Vector2(800, -350)
+		
+		
+			#handles attack trigger
+	if Input.is_action_just_pressed("Primary_Attack"):
+		current_state = player_states.ATTACK
+		
+		
+	if Input.is_action_just_pressed("Dash"):
+		current_state = player_states.DASH
+		
 	gravity_force()
 	move_and_slide()
 
 
+func wall_collide():
+	return wall.is_colliding()
+
+
 func gravity_force():
-	velocity.y += gravity
+	if !is_on_wall():
+		velocity.y += gravity
+	elif is_on_wall() && velocity.y > 0:
+		velocity.y += .5
+	else:
+		velocity.y += gravity
+		
+func dashing():
+	if velocity.x > 0:
+		velocity.x += dash_force
+		await get_tree().create_timer(0.1).timeout
+		current_state = player_states.MOVE
+	elif velocity.x < 0:
+		velocity.x -= dash_force
+		await get_tree().create_timer(0.1).timeout
+		current_state = player_states.MOVE
+	else:
+		if $AnimatedSprite2D.scale.x == 1:
+			velocity.x += dash_force
+			await get_tree().create_timer(0.1).timeout
+			current_state = player_states.MOVE
+		if $AnimatedSprite2D.scale.x == -1:
+			velocity.x -= dash_force
+			await get_tree().create_timer(0.1).timeout
+			current_state = player_states.MOVE
+	move_and_slide()
+		
+		
+func Attack(delta):
+	$Anim.play("Attack")
+	input_movement(delta)
+	
+func input_movement(delta):
+	input = Input.get_action_strength("Right") - Input.get_action_strength("Left")
+	#controlls the speed at which the character moves left and right with a fixed value
+	
+	if input != 0: 
+		if input > 0:
+			velocity.x += speed * delta
+			velocity.x = clamp(speed, 100.0, speed)
+			$AnimatedSprite2D.scale.x = 1
+		if input < 0:
+			velocity.x -= speed * delta
+			velocity.x = clamp(-speed, 100.0, -speed)
+			$AnimatedSprite2D.scale.x = -1
+	if input == 0:
+		velocity.x = 0 
+	gravity_force()
+	move_and_slide()
+
+	
+#returns the player to the move state and is set inside the Anim node under the Player function track
+func reset_state():
+	current_state = player_states.MOVE
